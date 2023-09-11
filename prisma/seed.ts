d0a5f7d4ab46466c95637a5681b6ce7dbb1c3dcc
John <johnari151298@gmail.com>
2023-09-11 11:35:08 +0200

    Quiosco Terminado

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import {categorias} from './DATA/categorias'
+import {productos} from './DATA/productos'
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient()
+
+const main = async () : Promise<void> => {
+    try{
+        await prisma.categoria.createMany({
+            data: categorias
+        })
+        await prisma.producto.createMany({
+            data: productos
+        })
+    }catch (error){
+        console.log(error);
+        
+    }
+}
+
+main()
